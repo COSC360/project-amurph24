7a15001979f4d49482a353288ed4862e8b42ebc7
zachpren0 <77454945+zachpren0@users.noreply.github.com>
2023-03-22 18:29:30 -0700

    Update test.php

--- database/test.php
+++ database/test.php
@@ -22,6 +22,17 @@ if ($conn->connect_error) {
 }
 echo "Connected successfully";
 
+$sql = "SELECT * FROM CATEGORY";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0){
+    while ($row = $result->fetch_assoc()){
+       echo "id: " . $row["category_id"];
+    }
+} else {
+    echo "0 results"
+}
+$conn->close();
 ?>
 </body>
 </html>
