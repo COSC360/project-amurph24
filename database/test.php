f888508549e7f563dadf963d2cbe2556c9d49325
zachpren0 <77454945+zachpren0@users.noreply.github.com>
2023-03-22 17:42:54 -0700

    Create test.php

---
+++ database/test.php
@@ -0,0 +1,21 @@
+<?php
+$url = 'https://cosc360.ok.ubc.ca/phpmyadmin'; // URL to access
+$username = 'your_username'; // Replace with your username
+$password = 'your_password'; // Replace with your password
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_USERPWD, "$username:$password"); // Set username and password
+curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // Ignore SSL verification (not recommended)
+$response = curl_exec($ch);
+
+if ($response === false) {
+  echo 'Error: ' . curl_error($ch);
+} else {
+  echo $response;
+}
+
+curl_close($ch);
+?>
